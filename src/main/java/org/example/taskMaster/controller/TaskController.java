790d7ba841fd4ee81096b6dec624752cd53297ce
Rybalchenko Vladimir <rybalchenko.v4@gmail.com>
2025-02-11 18:32:33 +0300

    added method updateTask

--- src/main/java/org/example/taskMaster/controller/TaskController.java
+++ src/main/java/org/example/taskMaster/controller/TaskController.java
@@ -5,12 +5,10 @@ import org.example.taskMaster.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RestController("/tasks")
 public class TaskController {
 
     @Autowired
@@ -39,6 +37,17 @@ public class TaskController {
         Task task = service.getTaskById(id);
         model.addAttribute("task", task);
         return "task-form";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateTask(@PathVariable Long id, @ModelAttribute Task task, Model model) {
+        Task updateTask = service.updateTask(id, task);
+        if(updateTask != null) {
+            return "redirect:/tasks";
+        } else {
+            model.addAttribute("error", "Task not found");
+            return "task-form";
+        }
     }
 
     @GetMapping("/delete/{id}")
